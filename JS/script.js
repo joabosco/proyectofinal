9a5735318be9f11db169c5381fa8e1dfc1b69857
Joana Bosco <joana89.jb@gmail.com>
2023-11-16 02:48:59 -0300

    segundo commit

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// mensaje de bienvenida
+
+
+function mostrarBienvenida() {
+    alert("¡Bienvenido a Ecoboutique! Gracias por visitarnos.");
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    mostrarBienvenida();
+});
+
+
